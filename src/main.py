64098bb799628834308996644b9f4495e92babde
akobcukvolodimir-jpg <akobcukvolodimir-jpg@gmail.com>
2026-06-03 11:16:29 +0300

    first commit

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from tasks.router import router as task_router
 
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000", 
+    "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
 
@@ -17,12 +18,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, world!"}
+
 
 app.include_router(task_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
